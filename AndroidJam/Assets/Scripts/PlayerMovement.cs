c5744c3d578a71ece72324ca54aec4ed9b4ef6ef
Kyohei <kalops22.kikoo@gmail.com>
2019-05-06 13:50:51 +0200

    Creation du player movement controller

---
+++ AndroidJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x, 0, 0);
+    }
+}
