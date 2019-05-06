7c7a785639fcbab9a5305d428251e958015d5447
Kyohei <kalops22.kikoo@gmail.com>
2019-05-06 15:23:35 +0200

    Player controller amélioré
    Ajout de la gestion du saut et de la respiration
    AJout du suivi de la camera sur le joueur

--- AndroidJam/Assets/Scripts/PlayerMovement.cs
+++ AndroidJam/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public CharacterController2D controller;
+
+    public float speedPlayer = 1;
+    public float minAngleToMove = 0.1f;
+
+    private float horizontalMove = 0;
+    private bool jump = false;
+
+    private float widthScreen;
+    private float heightScreen;
+
     void Start()
     {
-        
+        widthScreen = Screen.width;
+        heightScreen = Screen.height;
     }
 
     void Update()
     {
-        transform.Translate(Input.acceleration.x, 0, 0);
+        //SI le téléphone penche assez on déplace le joueur
+        if (Mathf.Abs(Input.acceleration.x) > minAngleToMove)
+            horizontalMove = Input.acceleration.x * speedPlayer;
+        else
+            horizontalMove = 0;
+
+        //Si on detecte un touchScreen
+        if(Input.touches.Length > 0)
+        {
+            foreach (Touch touch in Input.touches) //Position (0,0) est en bas à gauche de l'écran
+            {
+                if(touch.position.x > widthScreen / 2) //Si on touche a droite de l'écran
+                {
+                    jump = true;
+                }
+                if(touch.position.x < widthScreen / 2)//Si on touche a gauche de l'écran
+                {
+                    Debug.Log("Respire Connard !");
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
     }
 }
