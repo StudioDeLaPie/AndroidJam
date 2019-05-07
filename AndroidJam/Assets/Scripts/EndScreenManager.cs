0c902513907920ebdbc57908db3da57d1585be20
Kentarosan <jacquin.yannis@gmail.com>
2019-05-07 18:50:30 +0200

    Création écran de fin

---
+++ AndroidJam/Assets/Scripts/EndScreenManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenManager : MonoBehaviour
+{
+    public void OpenFacebook()
+    {
+        Application.OpenURL("https://www.facebook.com/StudioDeLaPie");
+
+    }
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/StudioDeLaPie");
+    }
+
+    public void OpenItch()
+    {
+        Application.OpenURL("https://studio-de-la-pie.itch.io/");
+    }
+
+    public void GoBackMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
